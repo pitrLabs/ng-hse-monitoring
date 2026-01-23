34058d45f7c45e978f690b38dba7aba87e45ecf3
Michkail <pitermichkail@ymail.com>
2026-01-24 00:08:13 +0700

    fix: env in entry sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,9 @@
 
 cat <<EOF > /app/dist/ng-hse-monitoring/browser/env.js
 window.__env = {
-  API_URL: "${API_URL}"
+  API_URL: "${API_URL}",
+  HLS_SERVER_URL: "${HLS_SERVER_URL}",
+  MEDIA_SERVER_URL: "${MEDIA_SERVER_URL}"
 };
 EOF
 

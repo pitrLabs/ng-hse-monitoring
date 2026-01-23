61263bbc7256b4c12ca282b4d189081853ced7b1
Michkail <pitermichkail@ymail.com>
2026-01-23 18:39:44 +0700

    update: env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: (window as any).__env?.API_URL || 'http://localhost:8000'
+  apiUrl: (window as any).__env?.API_URL || 'http://localhost:8000',
+  mediaServerUrl: (window as any).__env?.MEDIA_SERVER_URL || 'http://localhost:8889',
+  hlsServerUrl: (window as any).__env?.HLS_SERVER_URL || 'http://localhost:8888'
 };
